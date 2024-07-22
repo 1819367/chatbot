3bb7a9376f990f1412c6e548a2b9fa8e021b23c6
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-07-22 16:04:18 -0700

    Initial commit

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+user_input = input("\nAsk somting...\n\n")
+
+model="gpt-3.5-turbo"
+
+messages = [
+    {"role": "system", "content": "You are an assistant that always answers in the form of a poem."}, 
+    {"role": "user", "content": user_input}
+    ]
+
+response = client.chat.completions.create(
+   model=model,
+   messages=messages
+)
+response_for_user = response.choices[0].message.content
+
+print("\n" + response_for_user + "\n")
