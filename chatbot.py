414fc28defcfa4ddab19ed7abe23e462277b2bd9
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-07-22 17:25:48 -0700

    Refactored python code

--- chatbot.py
+++ chatbot.py
@@ -2,19 +2,34 @@ from openai import OpenAI
 
 client = OpenAI()
 
+# accepts the preferred model and messages list definition
+# calls the openai api
+# returns the response content
+def get_api_chat_response_message(model, messages):
+
+   # call API
+   api_response = client.chat.completions.create(
+        model= model,
+        messages= messages
+)
+   # extracted response from api, assign it to a variable   
+   response_content = api_response.choices[0].message.content
+
+   # return the response content 
+   return response_content
+
+# user prompt to ask the question
 user_input = input("\nAsk somting...\n\n")
 
+# variables for the python function
 model="gpt-3.5-turbo"
 
-messages = [
+messages=[
     {"role": "system", "content": "You are an assistant that always answers in the form of a poem."}, 
     {"role": "user", "content": user_input}
-    ]
+]
 
-response = client.chat.completions.create(
-   model=model,
-   messages=messages
-)
-response_for_user = response.choices[0].message.content
+# call the funtion, assign it to the variable response_for_user
+response_for_user = get_api_chat_response_message(model, messages)
 
 print("\n" + response_for_user + "\n")
