9343ea2d19c68192ad45e78a8668164601b7827a
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-07-23 14:54:22 -0700

    Categorizing user input and adjusting output

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,18 @@ from openai import OpenAI
 
 client = OpenAI()
 
+# accepts the user content as a parameter
+# attemps to define what kind of input the content is
+def set_user_input_category(user_input):
+   # determine if input is a question, create a dictionary
+   question_keywords = ["who", "what", "when", "where", "why", "how", "?"]
+   # search the user_input string for a substring of one of the keywords
+   # set to lower case using .lower()
+   for keyword in question_keywords:
+      if keyword in user_input.lower():
+         return "question"
+   return "statement"
+
 # accepts the preferred model and messages list definition
 # calls the openai api
 # returns the response content
@@ -19,7 +31,7 @@ def get_api_chat_response_message(model, messages):
    return response_content
 
 # user prompt to ask the question
-user_input = input("\nAsk somting...\n\n")
+user_input = input("\nAsk something...\n\n")
 
 # variables for the python function
 model="gpt-3.5-turbo"
@@ -31,5 +43,9 @@ messages=[
 
 # call the funtion, assign it to the variable response_for_user
 response_for_user = get_api_chat_response_message(model, messages)
+
+# tailored the output to the user
+if set_user_input_category(user_input) == "question":
+   response_for_user = "Good question! " + response_for_user
 
 print("\n" + response_for_user + "\n")
